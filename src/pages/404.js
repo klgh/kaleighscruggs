0014a38f7c1c4b12fce8e68409cfa5e77c7b3c49
Kaleigh Scruggs <kaleighscruggs@gmail.com>
2019-06-14 12:07:17 -0400

    new portfolio site

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,18 @@
 import React from "react"
-
+import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Image from "../components/image"
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
     <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <p>You just hit a page that doesn&#39;t exist... the sadness! </p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+    <Link to="/">Go back home</Link>
   </Layout>
 )
 
--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,15 +1,14 @@
 import React from "react"
 import { Link } from "gatsby"
-
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const SecondPage = () => (
   <Layout>
-    <SEO title="Page two" />
+    <SEO title="Resume" />
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <Link to="/">back</Link>
   </Layout>
 )
 
