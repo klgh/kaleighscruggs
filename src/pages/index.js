e77d1695d1b434b9e8a53868b628a7f2eddeecd4
Kaleigh Scruggs <kaleigh.scruggs@northhighland.com>
2019-06-18 11:26:28 -0400

    added font awesome styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,12 @@ import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { fab } from "@fortawesome/free-brands-svg-icons"
+import { faCheckSquare, faCoffee } from "@fortawesome/free-solid-svg-icons"
+
+library.add(fab, faCheckSquare, faCoffee)
 
 const IndexPage = () => (
   <Layout>
@@ -10,11 +16,13 @@ const IndexPage = () => (
     <p>Welcome to my portfolio</p>
     <p>Check out my other sites</p>
     <p>thanks</p>
+    <br />
     <a href="https://dev.to/kaleigh">
-      <i class="fab fa-dev" title="kaleigh's DEV Profile"></i>
-    </a>
-
+      <FontAwesomeIcon title="kaleigh's DEV Profile" icon={["fab", "dev"]} />
+    </a>{" "}
+    <br />
     <Link to="/resume/">resume</Link>
+    <br />
   </Layout>
 )
 
