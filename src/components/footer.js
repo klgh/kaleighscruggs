6e83ffc943cbc6e556060e7249d95e8a9e91cea8
Kaleigh Scruggs <kaleighscruggs@gmail.com>
2019-07-13 19:42:10 -0400

    fixing error

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,4 +1,3 @@
-import { Link } from "gatsby"
 import React from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { library } from "@fortawesome/fontawesome-svg-core"
