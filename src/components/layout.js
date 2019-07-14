9fb32fd4155174e409be0d9626567e43eb218eb9
Kaleigh Scruggs <kaleighscruggs@gmail.com>
2019-07-13 20:14:51 -0400

    styles

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,7 +4,7 @@ import { StaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import Footer from "./footer"
-import "./layout.scss"
+import "../styles/styles.scss"
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -20,15 +20,7 @@ const Layout = ({ children }) => (
     render={data => (
       <>
         <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-            textAlign: `center`,
-          }}
-        >
+        <div className="mainBody">
           <main>{children}</main>
         </div>
         <Footer />
