4e3ed2cdd4c4255622948028815a4beab9183f8c
Kaleigh Scruggs <ks301927@northhighland.com>
2019-07-18 15:37:42 -0400

    fixed img

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,18 +2,30 @@ import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import Image from "../components/image"
+import Img from "gatsby-image"
 
-const NotFoundPage = () => (
+const NotFoundPage = props => (
   <Layout>
     <SEO title="404: Not found" />
     <h1>NOT FOUND</h1>
     <p>You just hit a page that doesn&#39;t exist... the sadness! </p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+      <Img fluid={props.data.saddog.childImageSharp.fluid} />
     </div>
     <Link to="/">Go back home</Link>
   </Layout>
 )
 
 export default NotFoundPage
+
+export const pageQuery = graphql`
+  query {
+    saddog: file(relativePath: { eq: "saddog.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 600) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,12 +1,26 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Img from "gatsby-image"
 
-const Portfolio = () => (
+const Portfolio = props => (
   <Layout>
     <SEO title="Portfolio" />
     <h1>Portfolio</h1>
+    <Img fluid={props.data.saddog.childImageSharp.fluid} />
   </Layout>
 )
 
 export default Portfolio
+
+export const pageQuery = graphql`
+  query {
+    saddog: file(relativePath: { eq: "saddog.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 600) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
