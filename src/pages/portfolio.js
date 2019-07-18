930e4ca17a90e7b48cefed37d98b4e763280d247
Kaleigh Scruggs <ks301927@northhighland.com>
2019-07-18 17:19:39 -0400

    portfolio page

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -7,7 +7,71 @@ const Portfolio = props => (
   <Layout>
     <SEO title="Portfolio" />
     <h1>Portfolio</h1>
-    <Img fluid={props.data.saddog.childImageSharp.fluid} />
+    <h2>Freelance Sites</h2>
+    <div class="kelundra">
+      <a href="https://kelundra.com/" target="_blank" rel="noopener noreferrer">
+        <Img fluid={props.data.kelundra.childImageSharp.fluid} />
+      </a>
+      <h4 class="portfolioTitle">
+        <a
+          href="https://kelundra.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Kelundra Smith
+        </a>
+      </h4>
+    </div>
+    <div class="hawkpest">
+      <a href="https://hawkpest.com/" target="_blank" rel="noopener noreferrer">
+        <Img fluid={props.data.hawkpest.childImageSharp.fluid} />
+      </a>
+      <h4 class="portfolioTitle">
+        <a
+          href="https://hawkpest.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Hawk Pest Control
+        </a>
+      </h4>
+    </div>
+    <div class="yogaposies">
+      <a
+        href="http://yogaposies.com/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Img fluid={props.data.yogaposies.childImageSharp.fluid} />
+      </a>
+      <h4 class="portfolioTitle">
+        <a
+          href="http://yogaposies.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          YogaPosies
+        </a>
+      </h4>
+    </div>
+    <div class="laurensfloral">
+      <a
+        href="http://laurensfloralart.com/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Img fluid={props.data.laurensfloralart.childImageSharp.fluid} />
+      </a>
+      <h4 class="portfolioTitle">
+        <a
+          href="http://laurensfloralart.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Laurens Floral Art
+        </a>
+      </h4>
+    </div>
   </Layout>
 )
 
@@ -15,7 +79,28 @@ export default Portfolio
 
 export const pageQuery = graphql`
   query {
-    saddog: file(relativePath: { eq: "saddog.jpg" }) {
+    yogaposies: file(relativePath: { eq: "yogaposies.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 600) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    laurensfloralart: file(relativePath: { eq: "laurensfloralart.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 600) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    hawkpest: file(relativePath: { eq: "hawkpest.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 600) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    kelundra: file(relativePath: { eq: "kelundra.png" }) {
       childImageSharp {
         fluid(maxWidth: 600) {
           ...GatsbyImageSharpFluid
