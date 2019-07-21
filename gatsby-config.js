caa2d516875d1bdd18c4b2fc8591683369f11931
Kaleigh Scruggs <kaleighscruggs@gmail.com>
2019-07-21 16:42:15 -0400

    installing gatsby-transformer-remark

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,5 +60,20 @@ module.exports = {
         logo: "./src/images/favicon.png",
       },
     },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        // CommonMark mode (default: true)
+        commonmark: true,
+        // Footnotes mode (default: true)
+        footnotes: true,
+        // Pedantic mode (default: true)
+        pedantic: true,
+        // GitHub Flavored Markdown mode (default: true)
+        gfm: true,
+        // Plugins configs
+        plugins: [],
+      },
+    },
   ],
 }
