727dbe03be8c3f6a8b176077dff0860e2291377f
Kaleigh Scruggs <kaleighscruggs@gmail.com>
2019-07-27 23:19:53 -0400

    Update bloglayout.js

--- src/components/bloglayout.js
+++ src/components/bloglayout.js
@@ -6,7 +6,11 @@ import Footer from "./footer"
 import "../styles/styles.scss"
 
 <<<<<<< HEAD:src/components/bloglayout.js
+<<<<<<< HEAD:src/components/bloglayout.js
 const BlogLayout = ({ featuredImage, description, children }) => (
+=======
+const BlogPostLayout = ({ featuredImage, description, children }) => (
+>>>>>>> parent of 7b2973c... Update blog-post-layout.js:src/components/blog-post-layout.js
 =======
 const BlogPostLayout = ({ featuredImage, description, children }) => (
 >>>>>>> parent of 7b2973c... Update blog-post-layout.js:src/components/blog-post-layout.js
@@ -25,7 +29,11 @@ const BlogPostLayout = ({ featuredImage, description, children }) => (
       <>
         <Header siteTitle={data.site.siteMetadata.title} />
 <<<<<<< HEAD:src/components/bloglayout.js
+<<<<<<< HEAD:src/components/bloglayout.js
         <div>{children}</div>
+=======
+        <div className="blog-layout">{children}</div>
+>>>>>>> parent of 7b2973c... Update blog-post-layout.js:src/components/blog-post-layout.js
 =======
         <div className="blog-layout">{children}</div>
 >>>>>>> parent of 7b2973c... Update blog-post-layout.js:src/components/blog-post-layout.js
