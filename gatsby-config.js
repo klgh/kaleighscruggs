2d0ed63fe5c316113febe73b1d7353bb66a554e2
Kaleigh Scruggs <kaleigh.scruggs@northhighland.com>
2019-08-02 13:37:26 -0400

    Revert "removing slug"
    This reverts commit e28a34880c5b73890391c31b36a2e0358b3e3c4f.

--- gatsby-config.js
+++ gatsby-config.js
@@ -132,7 +132,8 @@ module.exports = {
                 return Object.assign({}, edge.node.frontmatter, {
                   description: edge.node.excerpt,
                   date: edge.node.frontmatter.date,
-                  url: site.siteMetadata.siteUrl + site.siteMetadata.siteUrl,
+                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
                   custom_elements: [{ "content:encoded": edge.node.html }],
                 })
               })
@@ -146,6 +147,7 @@ module.exports = {
                     node {
                       excerpt
                       html
+                      fields { slug }
                       frontmatter {
                         title
                         date
