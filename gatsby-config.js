9c5c1d6e69f0c37c2a0256f72d26ce0ae8c7f33b
Kaleigh Scruggs <kaleigh.scruggs@northhighland.com>
2020-01-29 12:45:50 -0500

    adding gatsby-plugin-offline, gatsby-plugin-feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,7 @@ module.exports = {
     `gatsby-plugin-sass`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sitemap`,
+
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -35,15 +36,16 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `kaleigh scruggs`,
-        short_name: `ks`,
+        short_name: `klgh.js`,
         start_url: `/`,
         background_color: `white`,
         theme_color: `#bd8e83`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`,
+        icon: `src/images/favicon.png`,
         image: `src/images/photo.jpg`,
       },
     },
+    `gatsby-plugin-offline`,
     {
       resolve: "gatsby-plugin-google-analytics",
       options: {
@@ -109,6 +111,60 @@ module.exports = {
         host: "https://kaleighscruggs.com",
         sitemap: "https://kaleighscruggs.com/sitemap.xml",
         policy: [{ userAgent: "*", allow: "/" }],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allWordpressPost } }) => {
+              return allWordpressPost.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  date: edge.node.date,
+                  url: site.siteMetadata.siteUrl + edge.node.slug,
+                  guid: site.siteMetadata.siteUrl + edge.node.slug,
+                })
+              })
+            },
+            query: `
+              {
+                allWordpressPost {
+                  edges {
+                    node {
+                      title
+                      excerpt
+                      slug
+                      date(formatString: "MMMM DD, YYYY")
+                    }
+                  }
+                }
+              }
+            `,
+            output: "/rss.xml",
+            title: "Kaleigh's Blog",
+            // optional configuration to insert feed reference in pages:
+            // if `string` is used, it will be used to create RegExp and then test if pathname of
+            // current page satisfied this regular expression;
+            // if not provided or `undefined`, all pages will have feed reference inserted
+            match: "^/blog/",
+            // optional configuration to specify external rss feed, such as feedburner
+            link: "https://feeds.feedburner.com/KaleighScruggs",
+          },
+        ],
       },
     },
   ],
