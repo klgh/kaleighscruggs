ae92e7e332074cc79a2aea9a35329f0b701ade8e
Kaleigh Scruggs <kaleighscruggs@gmail.com>
2020-12-01 17:56:33 -0500

    update

--- src/layouts/links-layout.js
+++ src/layouts/links-layout.js
@@ -1,13 +1,9 @@
 import React from "react"
-import Logo from "../components/logo"
-import Social from "../components/social"
 import "../styles/styles.scss"
 /* Link Page Layout */
 
 const LinksLayout = ({ children }) => (
   <div className="links-layout">
-    {/* <Logo siteTitle="klgh.js" /> */}
-    {/* <Social /> */}
     <main>{children}</main>
   </div>
 )
--- src/pages/links.js
+++ src/pages/links.js
@@ -3,6 +3,7 @@ import LinksLayout from "../layouts/links-layout"
 import SEO from "../components/seo"
 import { library } from "@fortawesome/fontawesome-svg-core"
 import { fab } from "@fortawesome/free-brands-svg-icons"
+import Logo from "../components/logo"
 
 library.add(fab)
 
@@ -10,6 +11,7 @@ const Links = () => (
   <LinksLayout>
     <SEO title="Links" />
     <div className="links">
+      <Logo siteTitle="klgh.js" />
       <p>
         <a href="https://www.etsy.com/shop/RollandCode">Roll &amp; Code</a>
       </p>
