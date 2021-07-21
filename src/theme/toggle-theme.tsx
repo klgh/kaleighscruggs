be5b688e5faad85ccfa2f5e782b1e4810fce324c
Kaleigh Scruggs <kaleighscruggs@gmail.com>
2021-07-21 09:23:54 -0400

    Update toggle-theme.tsx

--- src/theme/toggle-theme.tsx
+++ src/theme/toggle-theme.tsx
@@ -2,41 +2,33 @@ import React from 'react'
 import { Link } from 'gatsby'
 import { ThemeToggler } from 'gatsby-plugin-dark-mode'
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { library } from "@fortawesome/fontawesome-svg-core"
-import { fab } from "@fortawesome/free-brands-svg-icons"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
 
 library.add(fab)
 
 export default function ThemeToggle() {
   return (
     <div className="toggle_theme">
-      <a href="https://github.com/klgh">
-        <FontAwesomeIcon title="kaleigh's github" icon={["fab", "github"]} />
-      </a>{" "}
-      {/*  <Link to="https://www.instagram.com/klgh.js/">
-        <img as={FaInstagram} />
+      <Link to="https://www.instagram.com/klgh.js/">
+        <FontAwesomeIcon
+          title="kaleigh's instagram"
+          icon={['fab', 'instagram']}
+        />
       </Link>
       <Link to="https://github.com/klgh">
-        <img as={FaGithub} />
+        <FontAwesomeIcon title="kaleigh's github" icon={['fab', 'github']} />
       </Link>
       <Link to="https://dev.to/kaleigh">
-        <img as={FaDev} />
+        <FontAwesomeIcon title="kaleigh's github" icon={['fab', 'dev']} />
       </Link>
       <Link to="https://www.linkedin.com/in/kaleigh/">
-        <img as={FaLinkedin} />
+        <FontAwesomeIcon title="kaleigh's github" icon={['fab', 'linkedin']} />
       </Link>
       <Link to="https://twitter.com/kaleighscruggs">
-        <img as={FaTwitter} />
-      </Link> */}
-      {/* <imgButton
-        img={<Toggleimg />}
-        variant="ghost"
-        aria-label="Toggle Theme"
-        onClick={toggleMode}
-        w={6}
-        h={6}
-      /> */}
+        <FontAwesomeIcon title="kaleigh's github" icon={['fab', 'twitter']} />
+      </Link>
       <ThemeToggler>
         {({ theme, toggleTheme }) => (
           <label>
