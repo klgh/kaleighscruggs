ad524f3ce76c504dfabad89e429500dbc502e2e2
Kaleigh Scruggs <kaleigh.scruggs@sportsengine.com>
2021-08-02 15:25:11 -0400

    Revert "nav"
    This reverts commit a04c01d7ef2ab3ee11fd1b8b0a14601e78d48c9e.

--- src/components/header.js
+++ src/components/header.js
@@ -2,12 +2,10 @@ import React from 'react'
 import ThemeToggle from '../theme/toggle-theme'
 import Socials from './socials'
 import Logo from './logo'
-import Nav from './nav'
 
 const Header = ({}) => (
   <header>
     <Logo />
-    <Nav />
     <Socials />
     <ThemeToggle />
   </header>
