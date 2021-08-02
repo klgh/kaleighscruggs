a04c01d7ef2ab3ee11fd1b8b0a14601e78d48c9e
Kaleigh Scruggs <kaleighscruggs@gmail.com>
2021-08-02 13:55:33 -0400

    nav

---
+++ src/components/nav.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+export default function Nav() {
+  return (
+    <div className="socials">
+      <Nav>
+        <Link to={`/blog/`}>Blog</Link>
+      </Nav>
+    </div>
+  )
+}
