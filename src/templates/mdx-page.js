3ea9c20046c2fa90f7988585862f9e9c1fb272e3
Kaleigh Scruggs <kaleighscruggs@gmail.com>
2021-08-05 09:16:09 -0400

    links

--- src/templates/mdx-page.js
+++ src/templates/mdx-page.js
@@ -1,26 +1,30 @@
 import React from 'react'
 import { MDXProvider } from '@mdx-js/react'
-
 import Header from '../components/header'
-import { rhythm, typography } from '../utils/typography'
+import Footer from '../components/footer'
 
 function MDXPage({ children }) {
   return (
-    <MDXProvider
-      components={{
-        h1: (props) => <h2 {...props} className="heading2" />,
-        h2: (props) => <h3 {...props} className="heading3" />,
-        h3: (props) => <h4 {...props} className="heading4" />,
-        ul: (props) => <ul {...props} className="list-reset" />,
-        li: (props) => <li {...props} className="list-reset" />,
-        hr: (props) => <hr {...props} className="divider" />,
-        a: (props) => <a {...props} className="link" color="teal.500" />,
-        p: (props) => <p {...props} className="text-sm" fontSize="lg" />,
-      }}
-    >
+    <>
       <Header />
-      <div className="mdx_page"> {children}</div>
-    </MDXProvider>
+      <div className="mdx_page">
+        <MDXProvider
+          components={{
+            h1: (props) => <h2 {...props} className="heading2" />,
+            h2: (props) => <h3 {...props} className="heading3" />,
+            h3: (props) => <h4 {...props} className="heading4" />,
+            ul: (props) => <ul {...props} className="list-reset" />,
+            li: (props) => <li {...props} className="list-reset" />,
+            hr: (props) => <hr {...props} className="divider" />,
+            a: (props) => <a {...props} className="link" color="teal.500" />,
+            p: (props) => <p {...props} className="text-sm" />,
+          }}
+        >
+          {children}
+        </MDXProvider>
+        <Footer />
+      </div>
+    </>
   )
 }
 export default MDXPage
