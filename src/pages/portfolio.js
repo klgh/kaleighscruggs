aa397a28cf642299c43374217d420820ff491f5c
Kaleigh Scruggs <kaleighscruggs@gmail.com>
2021-09-04 18:23:55 -0400

    48in48 sites

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -12,49 +12,80 @@ const PortfolioPage = ({ data, location, props }) => {
       <SEO title="Portfolio" />
       <div className="portfolio">
         <h2>Portfolio</h2>
-        <h3>Freelance Sites</h3>
-        <div className="kelundra freelance_site">
-          <h4>Kelundra Smith</h4>
-          <a
-            href="https://kelundra.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <StaticImage src="../images/kelundra.png" alt="kelundra.com" />
-          </a>
+        <div className="freelance">
+          <h3>Freelance Sites</h3>
+          <div className="kelundra freelance_site">
+            <h4>Kelundra Smith</h4>
+            <a
+              href="https://kelundra.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <StaticImage src="../images/kelundra.png" alt="kelundra.com" />
+            </a>
+          </div>
+          <div className="hawkpest freelance_site">
+            <h4>Hawk Pest Control</h4>
+            <a
+              href="https://hawkpest.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <StaticImage src="../images/hawkpest.png" alt="hawkpest.com" />
+            </a>
+          </div>
+          <div className="yogaposies freelance_site">
+            <h4>YogaPosies</h4>
+            <a
+              href="http://yogaposies.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <StaticImage src="../images/yogaposies.png" alt="hawkpest.com" />
+            </a>
+          </div>
+          <div className="laurensfloral freelance_site">
+            <a
+              href="http://laurensfloralart.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h4>Laurens Floral Art</h4>
+              <StaticImage
+                src="../images/laurensfloralart.png"
+                alt="hawkpest.com"
+              />
+            </a>
+          </div>
         </div>
-        <div className="hawkpest freelance_site">
-          <h4>Hawk Pest Control</h4>
-          <a
-            href="https://hawkpest.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <StaticImage src="../images/hawkpest.png" alt="hawkpest.com" />
-          </a>
-        </div>
-        <div className="yogaposies freelance_site">
-          <h4>YogaPosies</h4>
-          <a
-            href="http://yogaposies.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <StaticImage src="../images/yogaposies.png" alt="hawkpest.com" />
-          </a>
-        </div>
-        <div className="laurensfloral freelance_site">
-          <a
-            href="http://laurensfloralart.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h4>Laurens Floral Art</h4>
-            <StaticImage
-              src="../images/laurensfloralart.png"
-              alt="hawkpest.com"
-            />
-          </a>
+        <div className="48in48">
+          <h3>48in48</h3>
+          <p>
+            <a href="https://48in48.org/">48in48</a> is a hackathon style
+            volunteer event where teams of marketing and technology
+            professionals are connected to local nonprofits to create brand new
+            WordPress websites in one weekend.
+          </p>
+          <div className="48in48">
+            Women's Build Event- April 9-11, 2021:{' '}
+            <a
+              href="https://communityvillagesdfw.org/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Community Villages DFW
+            </a>
+          </div>
+          <div>
+            Social Justice Event - June 11-13, 2021:{' '}
+            <a
+              href="https://auntiehelens.org/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Auntie Helen’s
+            </a>
+          </div>
         </div>
       </div>
     </Layout>
