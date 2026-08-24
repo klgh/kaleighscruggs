2c7066569685d010034211418ad9aa81c5b82cfd
Kaleigh Scruggs <kaleigh.scruggs@sportsengine.com>
2026-08-24 17:02:25 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import path from "node:path";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    host: "::",
+    host: '::',
     port: 8080,
     hmr: {
       overlay: false,
@@ -14,7 +14,7 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      "@": path.resolve(import.meta.dirname, "./src"),
+      '@': path.resolve(import.meta.dirname, './src'),
     },
   },
-});
+})
